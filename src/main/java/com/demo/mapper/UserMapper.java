473660ae99915b2eba6846fa8b4fad6fa43d138c
unknown <928288106@qq.con>
2020-01-24 16:17:30 +0800

    first

---
+++ src/main/java/com/demo/mapper/UserMapper.java
@@ -0,0 +1,33 @@
+package com.demo.mapper;
+
+import com.demo.bean.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public interface UserMapper {
+    /*
+     * 查询所有用户
+     */
+    List<User> userList();
+
+    /*
+     * 增加保存用户
+     */
+    void save(User user);
+
+    /*
+     * 根据id删除用户
+     */
+    int delete(Integer id);
+
+    /*
+     * 根据id查找用户
+     */
+    User findUserById(int id);
+
+    /*
+     * 更改用户信息
+     */
+    int update(User user);
+}
---
+++ src/main/java/com/demo/service/UserService.java
@@ -0,0 +1,50 @@
+package com.demo.service;
+
+import com.demo.bean.User;
+import com.demo.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserMapper userMapper; //注入UserMapper
+
+    /*
+     * 查询所有用户
+     */
+    public List<User> userList(){
+        return userMapper.userList();
+    }
+
+    /*
+     * 增加保存用户
+     */
+    public void save(User user){
+        userMapper.save(user);
+    }
+
+    /*
+     * 根据id删除用户
+     */
+    public int delete(Integer id){
+        return userMapper.delete(id);
+    }
+
+    /*
+     * 根据id查找用户
+     */
+    public User findUserById(int id){
+        return userMapper.findUserById(id);
+    }
+
+    /*
+     * 更改用户信息
+     */
+    public int update(User user){
+        return userMapper.update(user);
+    }
+}
